99d97316729e364954e90f9ec5a2c745bbf54868
Andreas Grams <andreas.grams@gmail.com>
2019-02-13 13:31:19 +0100

    make Spring boot app runnable by fixing configuration problem

--- src/main/java/de/holisticon/bewerber/grams/one/armed/bandit/Starter.java
+++ src/main/java/de/holisticon/bewerber/grams/one/armed/bandit/Starter.java
@@ -2,8 +2,10 @@ package de.holisticon.bewerber.grams.one.armed.bandit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(StarterConfig.class)
 public class Starter {
 
     public static void main(String[] args) {
--- src/main/java/de/holisticon/bewerber/grams/one/armed/bandit/StarterConfig.java
+++ src/main/java/de/holisticon/bewerber/grams/one/armed/bandit/StarterConfig.java
@@ -3,17 +3,10 @@ package de.holisticon.bewerber.grams.one.armed.bandit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.context.annotation.SessionScope;
 
 @Configuration
 public class StarterConfig {
-
-    @Bean
-    @RequestScope
-    public OneArmedBanditController oneArmedBanditController() {
-        return new OneArmedBanditController();
-    }
 
     @Bean
     @SessionScope(proxyMode = ScopedProxyMode.INTERFACES)
